61a3103d39e1eed771228b8ccc4c0b7e7ed4c419
Vishesh-dobhal <148621361+Vishesh-dobhal@users.noreply.github.com>
2025-05-14 13:04:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -93,8 +93,7 @@ import axios from "axios";
         "Bootstrap",
         "Responsive Design, python anywhere",
       ],
-      image:
-        "https://github.com/Vishesh-dobhal/my-portfolio/blob/main/src/assets/tweet.png",
+      image: require("./assets/tweet.png"),
       liveLink: "https://vaishdobhal.pythonanywhere.com/tweet/",
       githubLink: "https://github.com/Vishesh-dobhal/djangoproject.git",
     },
